99359298dd68a9a12a09d65b4fe6bb641e3b59dc
xmppnode <xmpp.node@gmail.com>
2022-01-29 00:00:05 -0800

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,15 @@
+import random 
+
 def main() -> None:
-  print('You rolled a die')
+  roll: int = random.randint(1,6)
+
+  dice_rolls: int = 2
+  dice_sum: int = 0
+  for i in range(0,dice_rolls):
+    roll: int = random.randint(1,6)
+    dice_sum += roll
+    print(f'You rolled a {roll}')
+  print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
